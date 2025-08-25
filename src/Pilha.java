55820309243a4318fc72f8bbae967630ea9c1969
Rodrigo Plassmann <rodrigo.plassmann@hotmail.com>
2025-08-24 22:16:15 -0300

    Criação de atributos e métodos para verificar se a pilha está cheia ou vazia

--- src/Pilha.java
+++ src/Pilha.java
@@ -2,10 +2,34 @@ public class Pilha {
     private int capacidade;
     private int topo;
     private int[] dados;
+    private boolean cheia;
+    private boolean vazia;
 
     public Pilha(int capacidade){
         this.capacidade = capacidade;
         this.topo = -1;
         this.dados = new int[this.capacidade];
+    }
+
+    public boolean vazia(){
+        if(this.topo == -1){
+            this.vazia = true;
+            return this.vazia;
+        }
+        else{
+            this.vazia = false;
+            return this.vazia;
+        }
+    }
+
+    public boolean cheia(){
+        if(this.topo == this.capacidade - 1){
+            this.cheia = true;
+            return this.cheia;
+        }
+        else{
+            this.cheia = false;
+            return this.cheia;
+        }
     }
 }
