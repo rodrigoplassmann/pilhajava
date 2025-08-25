8854c7c689416a43dd44547a3116c3e06848020d
Rodrigo Plassmann <rodrigo.plassmann@hotmail.com>
2025-08-24 22:09:14 -0300

    atributos e construtor da pilha

--- src/Pilha.java
+++ src/Pilha.java
@@ -1,2 +1,11 @@
 public class Pilha {
+    private int capacidade;
+    private int topo;
+    private int[] dados;
+
+    public Pilha(int capacidade){
+        this.capacidade = capacidade;
+        this.topo = -1;
+        this.dados = new int[this.capacidade];
+    }
 }
